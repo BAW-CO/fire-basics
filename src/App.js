efe8a4c9574890f380b006e973f1ea5618cb62bc
BAW-CO <bridgetw.designs@gmail.com>
2024-08-28 18:18:38 -0600

    creating FES Nav

--- src/App.js
+++ src/App.js
@@ -6,15 +6,22 @@ import { createUserWithEmailAndPassword,
   signOut,
   onAuthStateChanged,
  } from "firebase/auth";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+ import Nav from './components/Nav';
+import FESLogo from './assets/FES.svg';
+import bars from './assets/bars.svg';
 
 
 function App() {
   const [user, setUser] = React.useState({});
-  const [loading, setLoading] = React.useState(true);
+  const [isLoading, setIsLoading] = React.useState(false);
+  library.add(faBars);
 
   React.useEffect(() => {
     onAuthStateChanged(auth, (user) => {
-      setLoading(false);
       console.log(user);
       if (user) {
         setUser(user)
@@ -23,7 +30,7 @@ function App() {
   }, []);
 
   function register() {
-    createUserWithEmailAndPassword(auth, `email@email.com`, `test123`)
+    createUserWithEmailAndPassword(auth, `Bemail@email.com`, `test123`)
       .then((user) => {
         console.log(user)
       })
@@ -33,16 +40,18 @@ function App() {
   }
 
   function login() {
-    signInWithEmailAndPassword(auth, `email@email.com`, `test123`)
-    .then(({ user }) => {
-      console.log(user);
-      setUser(user);
-    })
-    .catch((error) => {
-      console.log(error.message);
-    })
+    setIsLoading(true);
+    signInWithEmailAndPassword(auth, `Bemail@email.com`, `test123`)
+      .then(({ user }) => {
+        console.log(user);
+        setUser(user);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error.message);
+        setIsLoading(false);
+      })
   }
-
   function logout() {
     signOut(auth);
     setUser({});
@@ -50,12 +59,47 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={register}>Register</button>
-      <button onClick={login}>Log In</button>
-      <button onClick={logout}>Log Out</button>
-      {loading ? 'loading...' : user.email}
+      <div className="nav">
+        <div className="nav__container">
+          <div className="nav__container-left">
+          
+            <button className="btn__menu">
+            <FontAwesomeIcon icon="bars" /> 
+            </button>
+            
+            <img src={FESLogo} alt="FES Logo" 
+            className="logo" />
+          </div>
+          <ul className="nav__links">
+            {!user.email && !isLoading && (
+              <>
+                <li className="nav__list">
+                  <button onClick={login}>Log In</button>
+                </li>  
+                <li className="nav__list">
+                  <button className="btn__register" onClick={register}>Register</button>
+                </li>
+              </>
+            )}
+            {isLoading && (
+              <li className="nav__list">
+                <div className="skeleton-loader"></div>
+                <div className="skeleton-loader-circle"></div>
+              </li>
+            )}
+            {user.email && !isLoading && (
+              <li className="nav__list">
+                <button className="btn__logout" onClick={logout}>
+                  {user.email[0].toUpperCase()}
+                </button>
+              </li>
+            )}
+          </ul>  
+          </div>
+        </div>
     </div>
   );
 }
-
 export default App;
+
+
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav>
+      {/* Add your navigation content here */}
+    </nav>
+  );
+}
+
+export default Nav;
